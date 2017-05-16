Fixed normalization of input variables and plotting of input variables

cms-open-data-ml/MLJEC_MCTruth_Model.py
@@ -7,8 +7,12 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
+#http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html#sklearn.preprocessing.StandardScaler
+#http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html#sklearn.decomposition.PCA
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+#http://scikit-learn.org/stable/tutorial/basic/tutorial.html#model-persistence
 from sklearn.externals import joblib
 from keras.models import Sequential, Model, model_from_json
 from keras.optimizers import SGD
@@ -18,6 +22,7 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from keras.callbacks import EarlyStopping
 from keras.layers import Merge, merge
 from keras import backend as K
+#https://docs.scipy.org/doc/numpy/reference/generated/numpy.clip.html
 import numpy as np
 import pandas as pd
 import sys, glob, argparse, h5py
@@ -25,6 +30,7 @@ from itertools import cycle
 from scipy import interp
 #from ipywidgets import FloatProgress
 #from IPython.display import display
+#https://github.com/tqdm/tqdm#latest-conda-release
 from tqdm import trange, tqdm
 from MLJEC_MCTruth_Util import rotate_and_reflect, prepare_df_dict, JetImageGenerator
 import MLJEC_MCTruth_Plot as plotter
@@ -117,7 +123,8 @@ def build_conv_model(nx=30, ny=30):
     output_layer = Dense(1, activation='linear', name='main_output')(layer)
     model = Model(input=[input_layer,jet_pt_ak7_input,jet_eta_ak7_input], output=output_layer)
     #model = Model(input=input_layer, output=output_layer)
-    model.compile(optimizer='adam', loss='kullback_leibler_divergence', metrics=['accuracy','precision','mse','msle'])
+    #model.compile(optimizer='adam', loss='kullback_leibler_divergence', metrics=['accuracy','precision','mse','msle'])
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy','precision','mse','msle'])
     return model
 
 def fitModels(df_dict_jet, df_dict_cand,nx,ny,generator,verbosity,debug):
@@ -143,7 +150,7 @@ def fitModels(df_dict_jet, df_dict_cand,nx,ny,generator,verbosity,debug):
                 #X_test = np.empty(16,dtype=object)
                 #f = FloatProgress(min=0, max=320)
                 #display(f)
-                for i in tqdm(range(960)):
+                for i in tqdm(range(12800)):
                     #f.value += 1
                     #X_train, encoded_Y_train = gen1.next()
                     if i==0:
@@ -190,14 +197,19 @@ def fitModels(df_dict_jet, df_dict_cand,nx,ny,generator,verbosity,debug):
             scaler = StandardScaler()
             X_train[1] = scaler.fit_transform(X_train[1].reshape(-1,1))
             X_test[1] = scaler.transform(X_test[1].reshape(-1,1))
+            #X_train[1] -= 200
+            #X_train[1] /= 100
+            #X_test[1] -= 200
+            #X_test[1] /= 100
+
             if not os.path.exists("scaler.pkl"):
                 joblib.dump(scaler, 'scaler.pkl') 
             #X_train[2] /= np.max(np.abs(X_train[2]),axis=0)
             #X_test[2] /= np.max(np.abs(X_test[2]),axis=0)
-            X_train[2] /= np.max(np.abs(5),axis=0)
-            X_test[2] /= np.max(np.abs(5),axis=0)
+            X_train[2] /= np.max(np.abs(2.5),axis=0)
+            X_test[2] /= np.max(np.abs(2.5),axis=0)
 
-            history = conv_model.fit(X_train, encoded_Y_train, validation_data=(X_test, encoded_Y_test), nb_epoch=30, batch_size=64, verbose=verbosity, callbacks=[early_stopping])
+            history = conv_model.fit(X_train, encoded_Y_train, validation_data=(X_test, encoded_Y_test), nb_epoch=100, batch_size=256, verbose=verbosity, callbacks=[early_stopping])
         histories.append(history)
         models.append(conv_model)
     return models, histories
@@ -243,7 +255,8 @@ def main(open_models,train_models,save_models,plot,generator,reset,debug,verbose
 
     #Make, load, and/or save the models
     if open_models:
-        models, histories = loadModel(verbose)
+        models = loadModel(verbose)
+        histories = []
     else:
         models, histories = fitModels(df_dict_jet,df_dict_cand,nx,ny,generator,verbose,debug)
     if save_models and len(models)>=1:
@@ -254,8 +267,10 @@ def main(open_models,train_models,save_models,plot,generator,reset,debug,verbose
         #plotter.plotJet(df_dict_jet, df_dict_cand,process='TT', njets_to_plot=1, nx=nx, ny=ny, xbins=xbins, ybins=ybins)
         #plotter.plotJet(df_dict_jet, df_dict_cand,process='QCD', njets_to_plot=1, nx=nx, ny=ny, xbins=xbins, ybins=ybins)
         #plotter.plot_ROC_curves(models[0])
-        plotter.plot_loss(histories)
-        plotter.plot_JES(models[0])
+        if len(histories)>0:
+            plotter.plot_loss(histories)
+        plotter.plot_JES(models[0],verbose)
+        #plotter.plot_inputs()
 
 if __name__ == '__main__':
     #program name available through the %(prog)s command

cms-open-data-ml/MLJEC_MCTruth_Plot.py
@@ -1,4 +1,5 @@
 from sklearn.externals import joblib
+from sklearn.preprocessing import scale
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -69,11 +70,11 @@ def plotJet(df_dict_jet, df_dict_cand, process='TT',njets_to_plot=-1, nx=30, ny=
     plt.ylabel('phi')
     plt.show()
 
-def plot_JES(conv_model):
+def plot_JES(conv_model, verbose):
     colors = cycle(['seagreen','cyan', 'indigo', 'yellow', 'blue', 'darkorange', 'red', 'black', 'green', 'brown'])
     #Plot the ROC curves for the training above
     for cv, color in zip(range(0,1), colors):
-        nbatches = 100
+        nbatches = 200
         jetImageGenerator2 = JetImageGenerator(2)
         gen = jetImageGenerator2.generator(test=True)
         y_predict = []
@@ -85,26 +86,43 @@ def plot_JES(conv_model):
         #f = FloatProgress(min=0, max=maxval)
         #display(f)
 
+        all_pT = np.array([])
+        all_eta = np.array([])
         for i in tqdm(range(nbatches)):
             #f.value += 1
             #if i%10==0:
             #    print "Jet",i
             Xp, yp = gen.next()
+            #if verbose:
+                #print ("Before transformation:")
+                #print ("\t",Xp)
 
             #Do all of the scaling
             np.clip(Xp[0],0,100,out=Xp[0])
             scaler = joblib.load('scaler.pkl')
             Xp[1] = scaler.transform(Xp[1].reshape(-1,1))
-            Xp[2] /= np.max(np.abs(5),axis=0)
+            #Xp[1] -= 200
+            #Xp[1] /= 100
+            Xp[2] /= np.max(np.abs(2.5),axis=0)
+
+            #if verbose:
+                #print ("After transformation:")
+                #print ("\t",Xp)
+                #print ("y_predict",yp)
+                #print ("y_score", conv_model.predict(Xp))
 
             y_predict += [yp]
             y_score += [conv_model.predict(Xp)]
+            all_pT = np.concatenate([all_pT.reshape(-1,1), Xp[1]])
+            all_eta = np.concatenate([all_eta.reshape(-1,1), Xp[2]])
+
         y_predict = np.concatenate(y_predict)
         y_score = np.concatenate(y_score)
         print y_predict
         print y_score
+    plt.figure()
     plt.scatter(y_predict, y_score, color=color, label='CNN')
-    plt.xlim([0.0, 3.0])
+    plt.xlim([0.5, 1.5])
     plt.ylim([0.0, 3.0])
     plt.ylabel('Predicted JEC')
     plt.xlabel('True JEC')
@@ -112,6 +130,24 @@ def plot_JES(conv_model):
     plt.legend(loc="lower right")
     plt.savefig("JES.png")
     #plt.show()
+
+    plt.figure()
+    plt.xlabel('pT')
+    plt.ylabel('Entries')
+    plt.title('Jet pT')
+    bins = np.linspace(-3, 3, 60)
+    plt.hist(all_pT,bins=bins, alpha=1, label='Mixed Sample',normed=True)
+    plt.legend(loc='upper right')
+    plt.savefig("pT.png")
+
+    plt.figure()
+    plt.xlabel('eta')
+    plt.ylabel('Entries')
+    plt.title('Jet eta')
+    bins = np.linspace(-1, 1, 20)
+    plt.hist(all_eta,bins=bins, alpha=1, label='Mixed Sample',normed=True)
+    plt.legend(loc='upper right')
+    plt.savefig("eta.png")
 
 def plot_ROC_curves(conv_model):
     colors = cycle(['cyan', 'indigo', 'seagreen', 'yellow', 'blue', 'darkorange', 'red', 'black', 'green', 'brown'])
@@ -152,6 +188,7 @@ def plot_ROC_curves(conv_model):
     mean_tpr /= kfold.get_n_splits(X, encoded_Y)
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
+    plt.figure()
     plt.plot(mean_fpr, mean_tpr, color='g', linestyle='--',label='Mean ROC (area = %0.2f)' % mean_auc, lw=lw)
     plt.xlim([0, 1.0])
     plt.ylim([0, 1.0])
@@ -255,4 +292,56 @@ def plot_loss(histories):
     plt.legend()
     plt.xlabel('epoch')
     plt.savefig("history.png")
+
+'''
+import MLJEC_MCTruth_Plot as plot
+plot.plot_inputs()
+'''
+def plot_inputs():
+    colors = cycle(['seagreen','cyan', 'indigo', 'yellow', 'blue', 'darkorange', 'red', 'black', 'green', 'brown'])
+    #Plot the ROC curves for the training above
+    for cv, color in zip(range(0,1), colors):
+        nbatches = 200
+        jetImageGenerator2 = JetImageGenerator(2)
+        gen = jetImageGenerator2.generator(test=True)
+
+        all_pT = np.array([])
+        all_eta = np.array([])
+        maxval = nbatches
+        for i in tqdm(range(nbatches)):
+            Xp, yp = gen.next()
+            #Do all of the scaling
+            scaler = joblib.load('scaler.pkl')
+            Xp[1] = scaler.transform(Xp[1].reshape(-1,1))
+            #Xp[1] -= 200
+            #Xp[1] /= 100
+            Xp[2] /= np.max(np.abs(2.5),axis=0)
+
+            #all_pT = np.concatenate([all_pT.reshape(-1,1), Xp[1]])
+            all_pT = np.concatenate([all_pT, Xp[1]])
+            all_eta = np.concatenate([all_eta, Xp[2]])
+
+    plt.figure()
+    plt.xlabel('pT')
+    plt.ylabel('Entries')
+    plt.title('Jet pT')
+    bins = np.linspace(-3, 3, 60)
+    plt.hist(all_pT,bins=bins, alpha=1, label='Mixed Sample',normed=True)
+    plt.legend(loc='upper right')
+    plt.savefig("pT.png")
+
+    plt.figure()
+    plt.xlabel('eta')
+    plt.ylabel('Entries')
+    plt.title('Jet eta')
+    bins = np.linspace(-1, 1, 20)
+    plt.hist(all_eta,bins=bins, alpha=1, label='Mixed Sample',normed=True)
+    plt.legend(loc='upper right')
+    plt.savefig("eta.png")
+
+
+
+
+
+  
 