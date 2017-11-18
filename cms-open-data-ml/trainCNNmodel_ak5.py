023e9ffd3cf943789a108ae80ce685c633da01be
EC2 Default User <ec2-user@ip-172-31-34-41.us-west-2.compute.internal>
2017-11-18 16:26:36 +0000

    update to train with jec_truth as target

--- cms-open-data-ml/trainCNNmodel_ak5.py
+++ cms-open-data-ml/trainCNNmodel_ak5.py
@@ -18,33 +18,43 @@ parser.add_option("-o", "--outFile", dest="output_filename",
 
 (options, args) = parser.parse_args()
 os.environ['KERAS_BACKEND'] = 'tensorflow'
-model = loadModel(options.model_filename)
-model.summary()
 
+print "LOADING DATA"
 df = pd.read_pickle(options.input_filename)
 
+print "COMPUTING JES_TRUTH"
+df['jes_truth'] = df.apply(lambda row: row['jet_pt']/row['gen_pt'] if row['jet_gen_dr']<0.25 else -1.,axis=1)
+print "SELECTING GOOD JETS"
+df = df.loc[(df['jes_truth']>0.)&(df['jes_truth']<2.)&(df['ak5pfcand_ijet']<2)]
+
+print "TRANSFORMING ETA/PT DISTRIBUTIONS"
 scaler = StandardScaler()
 df['jet_eta_scaled'] = df['jet_eta'] / 2.5
 df['jet_pt_exp'] = map(np.log,df['jet_pt'])
 df['jet_pt_scaled'] = scaler.fit_transform(df['jet_pt_exp'].reshape(-1, 1))
-df.head()
+print df.head()
 
+print "BUILDING TEST/TRAIN DATASET"
 df = df.sample(frac=1)
 df_train,df_test = np.array_split(df,2)
 
+print "REFORMATTING"
 jet_image_train = np.array(map(lambda x : x[0] , df_train['jet_image']))
 inputs_train = [jet_image_train.reshape([-1,30,30,1]),np.array(df_train['jet_pt_scaled']),np.array(df_train['jet_eta_scaled'])]
-
 jet_image_test = np.array(map(lambda x : x[0] , df_test['jet_image']))
 inputs_test = [jet_image_test.reshape([-1,30,30,1]),np.array(df_test['jet_pt_scaled']),np.array(df_test['jet_eta_scaled'])]
+
+print "LOADING MODEL"
+model = loadModel(options.model_filename)
+model.summary()
 
 model.compile(optimizer='adam', loss='mse', metrics=['accuracy','mse','msle'])
 
 from keras.callbacks import EarlyStopping 
 early_stopping = EarlyStopping(monitor='val_loss', patience=20)
 
-history=model.fit(inputs_train, np.array(df_train['jet_jes']), validation_data=(inputs_test, np.array(df_test['jet_jes'])), 
-                    nb_epoch=100, batch_size=1024, verbose=1, callbacks=[early_stopping])
+history=model.fit(inputs_train, np.array(df_train['jes_truth']), validation_data=(inputs_test, np.array(df_test['jes_truth'])), 
+                    nb_epoch=100, batch_size=1024, verbose=1)#, callbacks=[early_stopping])
 
 val_loss = np.asarray(history.history['val_loss'])
 loss = np.asarray(history.history['loss'])
@@ -58,7 +68,7 @@ plt.draw()
 fig.savefig('ak5_training_summary.png',dpi=100)
 
 model_json = model.to_json()
-with open(options.ouput_filename+".json", "w") as json_file:
+with open(options.output_filename+".json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
 model.save_weights(options.output_filename+".h5")
